2b5a99e432d81340d9b0e7c1acdd73c3a3df3be2
AndrewMurphy <amurf26@outlook.com>
2022-09-01 17:02:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-let number1 = parseInt(prompt("Enter a decimal number"));
+// let number1 = parseInt(prompt("Enter a decimal number"));
 // prompts for a number
 
-let result1 = number1.toString(2);
-// converts number into a string in base 2
+// let result1 = number1.toString(2);
+// // converts number into a string in base 2
 
-let result2 = number1.toString(16);
-alert(`Binary: ${result1}
-Hexadecimal: ${result2} `);
+// let result2 = number1.toString(16);
+// alert(`Binary: ${result1}
+// Hexadecimal: ${result2} `);
 // converts number into a string in base 16
+
+function hexa(){
+    let num = 546;
+    let t = true;
+    for(let i = 0; i<num; i++){
+        num = Math.floor(num/16);
+        console.log(num)
+    }
+}
+
+hexa();
